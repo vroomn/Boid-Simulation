f3f9deadfc34ce27b9baf065ff292d48100d7fd6
Vroom <harrison.hilpert@gmail.com>
2023-11-01 23:07:16 -0700

    Some organization

--- main.c
+++ main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
+
 #define GLFW_INCLUDE_VULKAN
 #include "GLFW/glfw3.h"
 
 int main(void) {
+    //Initalization of libraries
+    //Begin the GLFW library
     if (!glfwInit())
     {
         printf("failed");
+        return -1;
     }
 
-
-    if (glfwVulkanSupported()) {
-        printf("worked");
+    //Simple check to see if Vulkan is accessible on the device and build system
+    if (!glfwVulkanSupported()) {
+        printf("Requires Vulkan to display grpahics, please check drivers and SDK if in development\n");
+        return -1;
     }
 
+    //Main code
+
+
+    //Cleanup GLFW
     glfwTerminate();
 
     return 0;
